0ac2014d47c57322aa19d26cc496c246cafbaa04
Niels Thykier <nit@globeteam.com>
2023-06-02 15:29:02 +0200

    Verify endorsement chain is ordered correctly
    In the next version of the standard, sender is expected to send the
    list in order. Previously, you had to reconstruct the order as a
    receiver, which was just needlessly complex for little added value.
    
    Signed-off-by: Niels Thykier <nit@globeteam.com>

---
+++ ec-service/src/main/java/org/dcsa/endorsementchain/exceptions/TransferException.java
@@ -0,0 +1,8 @@
+package org.dcsa.endorsementchain.exceptions;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class TransferException extends RuntimeException {
+
+}
--- ec-service/src/test/java/org/dcsa/endorsementchain/service/ImportServiceTest.java
+++ ec-service/src/test/java/org/dcsa/endorsementchain/service/ImportServiceTest.java
@@ -1,29 +1,29 @@
 package org.dcsa.endorsementchain.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.dcsa.endorsementchain.datafactories.EBLEnvelopeTODataFactory;
 import org.dcsa.endorsementchain.datafactories.EndorsementChainEntryDataFactory;
 import org.dcsa.endorsementchain.datafactories.EndorsementChainEntryTODataFactory;
 import org.dcsa.endorsementchain.datafactories.SignedEndorsementChainEntryTODataFactory;
-import org.dcsa.endorsementchain.datafactories.EBLEnvelopeTODataFactory;
+import org.dcsa.endorsementchain.exceptions.BadEnvelopeException;
 import org.dcsa.endorsementchain.persistence.entity.EndorsementChainEntry;
+import org.dcsa.endorsementchain.transferobjects.EBLEnvelopeTO;
 import org.dcsa.endorsementchain.transferobjects.EndorsementChainEntryTO;
 import org.dcsa.endorsementchain.transferobjects.SignedEndorsementChainEntryTO;
-import org.dcsa.endorsementchain.transferobjects.EBLEnvelopeTO;
 import org.dcsa.endorsementchain.unofficial.service.TransportDocumentService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ImportServiceTest {
@@ -62,6 +62,34 @@ class ImportServiceTest {
     Optional<String> signedResponse = importService.importEbl(eblEnvelope);
     assertTrue(signedResponse.isPresent());
     assertEquals("dummyHash", signedResponse.get());
+  }
+
+
+  @Test
+  void testImportEblReorderedEndorsementChainEntries() {
+    List<EndorsementChainEntry> endorsementChainEntries = new ArrayList<>(EndorsementChainEntryDataFactory.getEndorsementChainEntryList());
+    assertTrue(endorsementChainEntries.size() > 1);
+    Collections.reverse(endorsementChainEntries);
+    List<String> rawEndorsementChainEntries = endorsementChainEntries.stream().map(parsedEndorsementChainEntry -> {
+      try {
+        return mapper.writeValueAsString(parsedEndorsementChainEntry);
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException("Can't serialize EndorsementChainEntry");
+      }
+    }).toList();
+    List<SignedEndorsementChainEntryTO> signedEndorsementChainEntries = SignedEndorsementChainEntryTODataFactory.signedEndorsementChainEntryTOList();
+
+    EBLEnvelopeTO eblEnvelope = EBLEnvelopeTODataFactory.eblEnvelopeTO(rawEndorsementChainEntries.get(0), rawEndorsementChainEntries.get(1));
+    List<EndorsementChainEntryTO> endorsementChainEntryTOs = EndorsementChainEntryTODataFactory.endorsementChainEntryTOList();
+
+    when(endorsementChainEntryService.verifyEndorsementChainSignature(signedEndorsementChainEntries.get(0).signature()))
+      .thenReturn(endorsementChainEntryTOs.get(0));
+    when(endorsementChainEntryService.verifyEndorsementChainSignature(signedEndorsementChainEntries.get(1).signature()))
+      .thenReturn(endorsementChainEntryTOs.get(1));
+    when(endorsementChainEntryService.signedEndorsementEntryToEndorsementChainEntry(signedEndorsementChainEntries.get(0), endorsementChainEntryTOs.get(0), eblEnvelope.document(), "localhost:8443")).thenReturn(endorsementChainEntries.get(0));
+    when(endorsementChainEntryService.signedEndorsementEntryToEndorsementChainEntry(signedEndorsementChainEntries.get(1), endorsementChainEntryTOs.get(1), eblEnvelope.document(), "localhost:8443")).thenReturn(endorsementChainEntries.get(1));
+
+    assertThrows(BadEnvelopeException.class, () -> importService.importEbl(eblEnvelope));
   }
 
   @Test
