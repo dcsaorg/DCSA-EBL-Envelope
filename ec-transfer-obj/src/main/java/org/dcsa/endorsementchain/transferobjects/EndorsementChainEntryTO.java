b6e68d222e21acc15af23407de34f4523a94b216
Niels Thykier <nit@globeteam.com>
2023-06-06 14:16:06 +0200

    Remove incorrect `@NotNull` annotation
    Signed-off-by: Niels Thykier <nit@globeteam.com>

--- ec-transfer-obj/src/main/java/org/dcsa/endorsementchain/transferobjects/EndorsementChainEntryTO.java
+++ ec-transfer-obj/src/main/java/org/dcsa/endorsementchain/transferobjects/EndorsementChainEntryTO.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record EndorsementChainEntryTO(
-  @NotNull  // TODO: This is wrong; it is nullable in some cases. Will fix in a later commit.
   @Size(max = 64, min = 64)
   String previousEnvelopeHash,  // TODO: Rename when we are ready to change the API
   @NotNull
